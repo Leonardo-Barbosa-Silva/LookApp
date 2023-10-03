7f1eb72c20fd29da99a3a15935941195f85a4d39
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-10-02 22:13:21 -0300

    home page already done

---
+++ client/src/pages/Product/index.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+const Container = styled.section`
+    width: 100%;
+    height: 100%;
+`
+
+function index() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default index
